aebc7ffa22c3d0b580c05a0e8ea83ae2d32f9316
Nano1408 <fernandowjose2000@gmail.com>
2024-07-05 21:51:46 -0500

    add component of developer

---
+++ src/process/Developer.jsx
@@ -0,0 +1,26 @@
+import { LiaLaptopCodeSolid } from "react-icons/lia";
+import { BiCodeBlock } from "react-icons/bi";
+
+const Developer = () => {
+  return (
+    <div className="cards developer_container_card flex justify-between mt-[30px]">
+        <div className="w-[400px]">
+            <div className='required flex mb-4'>
+                <h4>Desarrollo & Ejecución:</h4>
+                <span>
+                    <BiCodeBlock className="w-10 h-10 ml-4"/>
+                </span>
+            </div>
+            <p>
+                Usando tecnologías como React, Angular, Astro y Vue para facilitar el mantenimiento, 
+                desarrollaré páginas animadas que deleitarán a sus usuarios. Se integra Google 
+                Analytics, se realiza SEO y, después de algunos tutoriales, estará listo para 
+                comenzar.
+            </p>
+        </div>
+        <LiaLaptopCodeSolid className="icon_large w-[130px] h-[130px]"/>
+    </div>
+  )
+}
+
+export default Developer
