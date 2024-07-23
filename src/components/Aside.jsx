6b34c1cbfe2bdeac3b694469e429f4bc7455c6a8
Nano1408 <fernandowjose2000@gmail.com>
2024-07-23 16:24:49 -0500

    update responsive portfolio

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -7,6 +7,7 @@ import { TiSocialLinkedinCircular } from "react-icons/ti";
 import { TiSocialGithubCircular } from "react-icons/ti";
 import { IoLogoInstagram } from "react-icons/io";
 import { TiSocialFacebookCircular } from "react-icons/ti";
+import Social from '../social/Social';
 
 const Aside = () => {
   return (
@@ -39,43 +40,7 @@ const Aside = () => {
             </section>
 
             {/* iconos sociales */}
-            <section className='flex mt-[54px] w-full justify-center'>
-                {/* icon linkedin social */}
-                <a 
-                href="https://www.linkedin.com/in/fernando-perez-florez/" 
-                target='blank_'
-                className='socialIcon mr-3'
-                >
-                    <TiSocialLinkedinCircular className='icon text-5xl text-white'/>
-                </a>
-
-                {/* icon github social */}
-                <a 
-                href="https://github.com/Nano1408" 
-                target='blank_' 
-                className='socialIcon mr-3'
-                >
-                    <TiSocialGithubCircular className='icon text-5xl text-white'/>
-                </a>
-
-                {/* icon instagram social */}
-                <a 
-                href="https://www.instagram.com/fernadev14/" 
-                target='blank_' 
-                className='socialIcon mr-3'
-                >
-                    <IoLogoInstagram className='icon text-5xl text-white'/>
-                </a>
-
-                {/* icon facebook social */}
-                <a 
-                href="https://www.facebook.com/fernando.pf.3956" 
-                target='blank' 
-                className='socialIcon mr-3'
-                >
-                    <TiSocialFacebookCircular className='icon text-5xl text-white'/>
-                </a>
-            </section>
+            <Social />
 
             {/* boton de trabajemos juntos */}
             <section className='w-full flex justify-center mt-5'>
