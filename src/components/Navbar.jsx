1fc96e641f3b24da37ba0d4c0a2a54896f1f2b65
Nano1408 <fernandowjose2000@gmail.com>
2024-07-23 16:24:01 -0500

    update responsive portfolio

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,60 +1,109 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import '@styles/navbar.css'
 import '@fontsource/lilita-one';
+import { IoIosArrowDropdownCircle } from "react-icons/io";
+import { RxHamburgerMenu } from "react-icons/rx";
 
 const Navbar = () => {
-    const [activeSection, setActiveSection] = useState('Inicio');
+    const [activeSection, setActiveSection] = useState('inicio');
     const [isScrolled, setIsScrolled] = useState(false);
+    const [isScrolling, setIsScrolling] = useState(false);
+    const [menuOpen, setMenuOpen] = useState(false);
 
     const handleScroll = () => {
-        const sections = ['Inicio', 'Sobre mí', 'Proyectos', 'Proceso', 'Contacto'];
-        let currentSection = 'Inicio';
-
-        const element = document.getElementById('section');
-
-        sections.forEach( section => {
-            if(element){
-                const offset = element.offsetTop - window.scrollY;
-    
-                if(offset <= 50 && offset > -element.offsetHeight){
-                    currentSection = section;
-                }
-            }
-        });
-        setActiveSection(currentSection);
-
         if (window.scrollY > 50) {
             setIsScrolled(true);
         } else {
             setIsScrolled(false);
         }
 
-    }
+        if (!isScrolling) {
+            const sections = document.querySelectorAll('section');
+            let currentSection = activeSection;
+
+            sections.forEach(section => {
+                const element = document.getElementById(section.id);
+                if (element) {
+                    const rect = element.getBoundingClientRect();
+                    const topVisible = rect.top >= 0 && rect.top <= window.innerHeight / 2;
+                    const bottomVisible = rect.bottom >= window.innerHeight / 2 && rect.bottom <= window.innerHeight;
+
+                    if (topVisible || bottomVisible) {
+                        currentSection = section.id;
+                    }
+                }
+            });
+
+            // Si estamos en la parte superior de la página
+            if (window.scrollY === 0 || window.scrollY < 250) {
+                currentSection = 'inicio';
+            }
+
+            setActiveSection(currentSection);
+        }
+    };
 
     useEffect(() => {
         window.addEventListener('scroll', handleScroll);
-        return () => removeEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, [isScrolling]);
+
+    useEffect(() => {
+        // Verificar si estamos en el inicio al cargar la página
+        if (window.scrollY === 0) {
+            setActiveSection('inicio');
+        }
     }, []);
 
     const handleClick = (section) => {
-        setActiveSection(section);
-        document.getElementById(section).scrollIntoView({behavior : 'smooth'})
-    }
+        setIsScrolling(true);
+        document.getElementById(section).scrollIntoView({ behavior: 'smooth' });
+
+        setTimeout(() => {
+            setActiveSection(section);
+            setIsScrolling(false);
+        }, 500); // Ajusta el tiempo para que coincida con la duración de la animación de desplazamiento
+    };
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
+    const navRef = useRef(null);
+
+    const handleClickOutside = (event) => {
+        if (navRef.current && !navRef.current.contains(event.target)) {
+            setMenuOpen(false);
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, []);
+
 
 return (
-    <nav className={`${isScrolled ? 'nav-scrolled' : ''}`}>
-        <ul className='flex'>
-            {['Inicio', 'Sobre mí', 'Proyectos', 'Proceso', 'Contacto'].map((section) => (
-                    <li
-                    key={section}
-                    className={`text-white text-xl rounded-lg px-4 ${
-                    activeSection === section ? 'selected' : 'unselected'
-                    }`}
-                    onClick={() => handleClick(section)}
-                    >
-                        {section}
-                    </li>
-            ))}
+    <nav ref={navRef} className={`${isScrolled ? 'nav-scrolled' : ''} ${menuOpen ? 'menu-open' : ''}`}>
+        <RxHamburgerMenu className={`toggle ${menuOpen ? 'open' : ''}`} onClick={toggleMenu}/>
+        <ul className={`container_ul_nav ${menuOpen ? 'open' : ''}`}>
+            <li className={activeSection === 'inicio' ? 'selected' : 'unselected'} onClick={() => handleClick('inicio')}>Inicio</li>
+            <li className="menu">
+                <div className="flex items-center">
+                    Sobre mí <IoIosArrowDropdownCircle className='arrow_icon pl-2 text-3xl'/>
+                </div>
+                <ul className={`subMenu rounded-none `}>
+                    <li className={activeSection === 'experiencia' ? 'selected' : 'unselected'} onClick={() => handleClick('experiencia')}>Experiencia</li>
+                    <li className={activeSection === 'education' ? 'selected' : 'unselected'} onClick={() => handleClick('education')}>Educación</li>
+                    <li className={activeSection === 'teams' ? 'selected' : 'unselected'} onClick={() => handleClick('teams')}>Equipo</li>
+                    <li className={activeSection === 'review' ? 'selected' : 'unselected'} onClick={() => handleClick('review')}>En cuanto...</li>
+                </ul>
+            </li>
+            <li className={activeSection === 'proyectos' ? 'selected' : 'unselected'} onClick={() => handleClick('proyectos')}>Proyectos</li>
+            <li className={activeSection === 'proceso' ? 'selected' : 'unselected'} onClick={() => handleClick('proceso')}>Proceso</li>
+            <li className={activeSection === 'contacto' ? 'selected' : 'unselected'} onClick={() => handleClick('contacto')}>Contacto</li>
         </ul>
     </nav>
 )
