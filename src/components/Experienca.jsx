a72a6e817eca6fa0247c31dd1121b78feac28b1a
fernadev14 <fernandoperez.99a@gmail.com>
2026-03-01 00:41:39 -0500

    cambios responsive

--- src/components/Experienca.jsx
+++ src/components/Experienca.jsx
@@ -8,9 +8,9 @@ const Experienca = () => {
   return (
     <div className="mt-20">
       {/* contexto */}
-      <section id="container_context">
+      <section id="container_context" className='mx-8'>
           <h2 className="text-4xl font-bold">Experiencia practica</h2>
-          <p>
+          <p className='text-neutral-400 mt-6 leading-relaxed mx-auto text-sm sm:text-base'>
               Desarrollé aplicaciones con React y Firebase optimizando tiempos de carga en un 30%, creando interfaces 
               responsivas y soluciones funcionales.
               Uso IA en mi día a día para análisis, generación de ideas, 
@@ -18,7 +18,7 @@ const Experienca = () => {
               consciente como desarrollador.
           </p>
       </section>
-      <section className="mt-16 grid grid-cols-1 lg:grid-cols-2 gap-10 justify-items-center">
+      <section className="card_experiencia mt-16 grid grid-cols-1 lg:grid-cols-2 gap-10 justify-items-center">
         {projects.map((project, index) => (
           <ProjectCard
             key={index}
@@ -27,20 +27,6 @@ const Experienca = () => {
           />
         ))}
       </section>
-        {/* card proyecto 3 (Huella de regreso) */}
-        {/* <HuellaDeRegreso /> */}
-
-        {/* card proyecto 3 (Huella de regreso) */}
-        {/* <Jardineria /> */}
-        
-        {/* card proyecto 4 (FernaShop - panel admin) */}
-        {/* <FernaShop /> */}
-
-        {/* card proyecto 1 (clima) */}
-        {/* <WeatherProject /> */}
-
-        {/* card proyecto 2 (album de musica) */}
-        {/* <AlbumMusicProject /> */}
 
     </div>
 );
